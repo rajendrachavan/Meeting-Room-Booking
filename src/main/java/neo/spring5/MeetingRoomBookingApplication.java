0e3f4781c215ef4f2de595982e04cfedc2e7204b
Rajendra Chavan <37338125+rajendrachavan@users.noreply.github.com>
2019-10-21 19:38:08 +0530

    Initial Commit Local Repo.

---
+++ src/main/java/neo/spring5/MeetingRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package neo.spring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neo/spring5/MeetingRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.spring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
