07c61c6809c15bb56100bcd3707ef0dfcc2eff71
Rajendra Chavan <37338125+rajendrachavan@users.noreply.github.com>
2019-10-21 21:07:20 +0530

    Refactored Save Method for User. closes #5

--- src/main/java/neo/spring5/MeetingRoomBooking/services/UserServiceImpl.java
+++ src/main/java/neo/spring5/MeetingRoomBooking/services/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package neo.spring5.MeetingRoomBooking.services;
 
+import neo.spring5.MeetingRoomBooking.models.Role;
+import neo.spring5.MeetingRoomBooking.repositories.RoleRepository;
 import neo.spring5.MeetingRoomBooking.repositories.UserRepository;
 import neo.spring5.MeetingRoomBooking.models.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,13 +12,20 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public User save(User user) {
+        user.setPasswd(bCryptPasswordEncoder.encode(user.getPasswd()));
+        Role userRole = roleRepository.findByRoleType("ADMIN");
+        user.setRole(userRole);
         return userRepository.save(user);
     }
 
