2b23b59f00a2298838068c48c9d58a3e4c36e862
rajendrachavan <rajendrachavan.jvm@gmail.com>
2019-10-25 15:06:15 +0530

    Registration & Login Working with ADMIN ROLE.

--- src/main/java/neo/spring5/MeetingRoomBooking/services/UserService.java
+++ src/main/java/neo/spring5/MeetingRoomBooking/services/UserService.java
@@ -3,8 +3,13 @@ package neo.spring5.MeetingRoomBooking.services;
 
 import neo.spring5.MeetingRoomBooking.models.User;
 
+import java.util.List;
+
 public interface UserService {
 
 	public User findUserByEmail(String email);
 	public void saveUser(User user);
+	public List<User> findAll();
+	public User findById(Long id);
+	public void deleteById(Long id);
 }
--- src/main/java/neo/spring5/MeetingRoomBooking/services/UserServiceImpl.java
+++ src/main/java/neo/spring5/MeetingRoomBooking/services/UserServiceImpl.java
@@ -2,6 +2,7 @@ package neo.spring5.MeetingRoomBooking.services;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import neo.spring5.MeetingRoomBooking.models.Role;
 import neo.spring5.MeetingRoomBooking.models.User;
@@ -35,4 +36,20 @@ public class UserServiceImpl implements UserService{
 		userRepository.save(user);
 	}
 
+	@Override
+	public List<User> findAll() {
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+
+	@Override
+	public User findById(Long id) {
+		User user = userRepository.findById(id).get();
+		return user;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
+	}
 }
